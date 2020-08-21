67a1c239b01144c385e219a7c894676cab6c0029
DwaynesWorld <kthompson713@gmail.com>
2020-08-21 16:45:33 -0500

    Initial Commit

---
+++ Kiddo/KiddoApp.swift
@@ -0,0 +1,18 @@
+//
+//  KiddoApp.swift
+//  Kiddo
+//
+//  Created by Kyle Thompson on 8/21/20.
+//  
+//
+
+import SwiftUI
+
+@main
+struct KiddoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
